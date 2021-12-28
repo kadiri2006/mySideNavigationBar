148e5a7b42969204cb75c7f0dfb908ee86228db5
Your Name <you@example.com>
2021-12-28 10:55:07 +0530

    final

---
+++ index.js
@@ -0,0 +1,8 @@
+let btn = document.getElementsByClassName("click")[0];
+let navbar = document.getElementsByClassName("nav_bar")[0];
+btn.addEventListener("click", function () {
+  navbar.classList.toggle("open");
+
+});
+
+
